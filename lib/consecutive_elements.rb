d942618ac98a636ab75341750364f3ea7554d4e7
marthaberner <marthaberner.mb@gmail.com>
2014-08-11 10:02:17 -0600

    Finds consecutive elements
    validates the schedule

--- lib/consecutive_elements.rb
+++ lib/consecutive_elements.rb
@@ -4,12 +4,25 @@ class ConsecutiveElements
     @schedule = schedule
   end
 
-  def into_array
+  def find_consecutives
     result = []
-    @schedule.each do |activity|
-      result << [activity]
+    length = @schedule.length
+    count = 1
+    length.times do |i|
+      curr_el = @schedule[i+1]
+      if curr_el != @schedule[i]
+        result << [@schedule[i], count]
+        count = 1
+      else
+        count += 1
+      end
     end
     result
   end
 
+  def schedule_valid?
+    find_consecutives[0..-2].all? { |activity| activity[1] >= 2 }
+  end
 end
+
+
--- spec/consecutive_elements_spec.rb
+++ spec/consecutive_elements_spec.rb
@@ -2,21 +2,37 @@ require 'rspec'
 require 'consecutive_elements'
 
 describe ConsecutiveElements do
-  it 'Turns array into an array of arrays' do
-    schedule = ["archery", "swimming", "swimming", "crafts", "archery", "archery", "crafts"]
-
-    input = ConsecutiveElements.new(schedule)
+  before do
+    @schedule = ["archery", "swimming", "swimming", "crafts", "archery", "archery", "crafts"]
+  end
+  it 'Produces an array of all the activities and the number of consecutive occurrences for that activity ' do
+    input = ConsecutiveElements.new(@schedule)
 
     expected = [
-      ["archery"],
-      ["swimming"],
-      ["swimming"],
-      ["crafts"],
-      ["archery"],
-      ["archery"],
-      ["crafts"]
+      ["archery", 1],
+      ["swimming", 2],
+      ["crafts", 1],
+      ["archery", 2],
+      ["crafts", 1]
     ]
 
-    expect(input.into_array).to eq(expected)
+    expect(input.find_consecutives).to eq(expected)
   end
-end
+
+    it 'Checks to see if a schedule is invalid' do
+
+      input = ConsecutiveElements.new(@schedule)
+
+      expect(input.schedule_valid?).to eq false
+    end
+
+    it 'Checks to see if a schedule is valid' do
+      schedule = ["archery", "archery", "swimming", "swimming", "archery", "archery", "crafts"]
+
+      input = ConsecutiveElements.new(schedule)
+
+      expect(input.schedule_valid?).to eq true
+    end
+
+
+  end
